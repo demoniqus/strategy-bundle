611ac34cb8c636f9570ce7fed6579dd44661fb4b
antipda <antipda@ite-ng.ru>
2022-01-18 08:29:41 +0300

    v.0.0.2

--- src/Repository/StrategyCommandRepositoryInterface.php
+++ src/Repository/StrategyCommandRepositoryInterface.php
@@ -7,7 +7,7 @@ namespace Evrinoma\StrategyBundle\Repository;
 interface StrategyCommandRepositoryInterface
 {
 //region SECTION:Public
-
+    public function save(): bool;
 //endregion Public
 //region SECTION: Getters/Setters
 
